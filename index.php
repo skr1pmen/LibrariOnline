df922859c3825492e770f0788e803ae51a27b8be
Skrip men <kiberkot.v.roymenge@gmail.com>
2023-02-15 16:34:38 +0400

    bg add

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ session_start()
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+    <img src="./images/background.jpg" alt="">
     <header>
         <div class="container">
             <a href="#" class="logo head"><span class="logo_text">Library Online</span></a>
